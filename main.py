b8dbfb54affd34c2a6cbaf64308dbbf33a564c01
vicccente5 <chubitomermoreno@gmail.com>
2025-09-04 14:00:41 -0400

    agenda.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#agenda 
+contacto = ()
+
+def agregar_contacto(nombre, telefono):
+    contacto.append(("nombre": nombre, "telefono": telefono))
+
+nombre = input("Ingrese el nombre del contacto: ")
+telefono = input("Ingrese el teléfono del contacto: ")
+agregar_contacto(nombre, telefono)
+print("Contacto agregado:", contacto)
